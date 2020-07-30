2f9337bae3fa1037dfb0cae1d263a1afcece05bd
ludruda <ludruda@microsoft.com>
2020-07-30 09:35:57 +0200

    v 1.0.5

--- setup.py
+++ setup.py
@@ -5,11 +5,14 @@ sys.path.pop(0)
 from setuptools import setup
 sys.path.append(".")
 import sdist_upip
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='micropython-iotc',
-      version='1.0.4',
+      version='1.0.5',
       description='Azure IoT Central client for MicroPython (mqtt protocol)',
-      long_description='',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/iot-for-all/iotc-micropython-client',
       author='Luca Druda',
       author_email='ludruda@microsoft.com',
